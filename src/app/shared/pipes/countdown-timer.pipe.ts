068ff2f094c3873d7db155db21026aa318ff64db
Nursultan Kadyrov <nur@roky.rocks>
2024-05-02 17:33:30 +0500

    feat: translate from rus to eng

--- src/app/shared/pipes/countdown-timer.pipe.ts
+++ src/app/shared/pipes/countdown-timer.pipe.ts
@@ -9,7 +9,7 @@ import { map, startWith, takeWhile } from 'rxjs/operators';
 export class CountdownTimerPipe implements PipeTransform {
   transform(time?: string): Observable<string> {
     if (!time || !time.trim()) {
-      return of('Нет данных');
+      return of('No data');
     }
 
     return interval(1000).pipe(
@@ -27,16 +27,16 @@ export class CountdownTimerPipe implements PipeTransform {
         const duration = targetTime.getTime() - now.getTime();
 
         if (duration < 0) {
-          return 'Время истекло';
+          return 'Time is left';
         }
 
         const hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
         const minutes = Math.floor((duration / (1000 * 60)) % 60);
         const seconds = Math.floor((duration / 1000) % 60);
 
-        return `${hours}ч ${minutes}м ${seconds}с`;
+        return `${hours}h ${minutes}m ${seconds}s`;
       }),
-      takeWhile((x) => x !== 'Время истекло', true)
+      takeWhile((x) => x !== 'Time is left', true)
     );
   }
 }
